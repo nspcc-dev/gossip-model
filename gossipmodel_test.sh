9216d6b22d1b5559bca90510b05a3f9722398afb
Karev Nikita <nikitaaak@outlook.com>
2020-06-19 00:40:08 +0300

    Script parameters added

--- gossipmodel_test.sh
+++ gossipmodel_test.sh
@@ -2,22 +2,22 @@
 
 NUM_TESTS=10
 
-SIZE=100 # Need to change
-FANOUT=15
-NUMEXP=10
-DEFAULT_PROB=1
+SIZE=10000 # Need to change
+FANOUT=10000
+NUMEXP=100
+DEFAULT_PROB=0.3
 SCENARIO_NUM=6
 
-LIMIT=10
+LIMIT=5
 
 for ((scenario=0; scenario < $SCENARIO_NUM; scenario++))
 do
-	for ((fanout=1; fanout < $FANOUT; fanout++))
+	for ((fanout=1; fanout <= $FANOUT; fanout++))
 	do
 		counter=0
 		for ((test_id=0; test_id < $NUM_TESTS; test_id++))
 		do
-			OUT=$(./gossipmodel -c $NUMEXP -p $DEFAULT_PROB -f $fanout -k 0.7/10 -k 0.9/20 -s $SIZE -e $scenario)
+			OUT=$(./gossipmodel -c $NUMEXP -p $DEFAULT_PROB -f $fanout -k 0.7/1000 -k 0.9/2000 -k 0.6/1000 -k 0.8/3000 -k 0.7/1000 -k 0.6/1000 -k 0.9/1000 -s $SIZE -e $scenario)
 			iter=$(echo "$OUT" | tail -n3 | head -n1 | awk '{print $1}' | awk -F ':' '{print$1}')
 			finexp=$(echo "$OUT" | tail -n3 | head -n1 | awk '{print $1}' | awk -F ':' '{print$2}')
 			restime=$(echo "$OUT" | tail -n1)
